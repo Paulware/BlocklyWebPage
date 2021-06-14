efad381e3ed371d6e2391f6c7b923bfb0daa421e
Paul Richards <paulware@hotmail.com>
2021-06-14 14:39:42 -0500

    update

--- docs/demos/code/blocks.js
+++ docs/demos/code/blocks.js
@@ -1,3 +1,30 @@
+/*
+   Overwrite block definitions
+   Note: See docs/blocks_compressed.js for changed Blockly.Blocks.procedures_defnoreturn
+*/
+Blockly.Blocks.procedures_defreturn={
+ init:function(){
+  var a=Blockly.Procedures.findLegalName("",this);
+  a=new Blockly.FieldTextInput(a,Blockly.Procedures.rename);
+  a.setSpellcheck(!1);
+  this.appendDummyInput().appendField(Blockly.Msg.PROCEDURES_DEFRETURN_TITLE).appendField(a,"NAME").appendField("","PARAMS");
+  this.appendValueInput("RETURN").setAlign(Blockly.ALIGN_RIGHT).appendField(Blockly.Msg.PROCEDURES_DEFRETURN_RETURN);
+  this.setMutator(new Blockly.Mutator(["procedures_mutatorarg"]));
+  (this.workspace.options.comments||this.workspace.options.parentWorkspace&&this.workspace.options.parentWorkspace.options.comments)&&Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT&&this.setCommentText(Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT);
+  this.setStyle("procedure_blocks");
+  this.setPreviousStatement(true, null);
+  this.setNextStatement(true, null);  
+  this.setTooltip(Blockly.Msg.PROCEDURES_DEFRETURN_TOOLTIP);
+  this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFRETURN_HELPURL);
+  this.arguments_=[];
+  this.argumentVarModels_=[];
+  this.setStatements_(!0);
+  this.statementConnection_=null},setStatements_:Blockly.Blocks.procedures_defnoreturn.setStatements_,
+   updateParams_:Blockly.Blocks.procedures_defnoreturn.updateParams_,mutationToDom:Blockly.Blocks.procedures_defnoreturn.mutationToDom,domToMutation:Blockly.Blocks.procedures_defnoreturn.domToMutation,decompose:Blockly.Blocks.procedures_defnoreturn.decompose,compose:Blockly.Blocks.procedures_defnoreturn.compose,getProcedureDef:function(){return[this.getFieldValue("NAME"),this.arguments_,!0]},getVars:Blockly.Blocks.procedures_defnoreturn.getVars,getVarModels:Blockly.Blocks.procedures_defnoreturn.getVarModels,
+   renameVarById:Blockly.Blocks.procedures_defnoreturn.renameVarById,updateVarName:Blockly.Blocks.procedures_defnoreturn.updateVarName,displayRenamedVar_:Blockly.Blocks.procedures_defnoreturn.displayRenamedVar_,customContextMenu:Blockly.Blocks.procedures_defnoreturn.customContextMenu,callType_:"procedures_callreturn"
+ };
+
+
 Blockly.Blocks['html'] = {
   init: function() {
     this.appendDummyInput()
@@ -17,7 +44,11 @@ Blockly.Blocks['body'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Body Components");
-
+                
+    this.appendDummyInput()
+        .appendField("on load:")
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
+        
     this.appendStatementInput("BODY")
         .setCheck(null);
     this.setPreviousStatement(true, null);
@@ -106,7 +137,7 @@ Blockly.Blocks['checkbox'] = {
         
     this.appendDummyInput()
         .appendField("on click call:")
-        .appendField (new Blockly.FieldTextInput ("fun1(this.value)"), "CALL");         
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
 
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -115,3 +146,379 @@ Blockly.Blocks['checkbox'] = {
     this.setHelpUrl('http://www.example.com/');
   }
 };
+
+Blockly.Blocks['slider'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Slider id:")
+        .appendField (new Blockly.FieldTextInput ("id"), "ID"); 
+        
+    this.appendDummyInput()
+        .appendField("Minimum")
+        .appendField (new Blockly.FieldNumber (1.0), "MINIMUM"); 
+        
+    this.appendDummyInput()
+        .appendField("Maximum")
+        .appendField (new Blockly.FieldNumber (100.0), "MAXIMUM");         
+        
+    this.appendDummyInput()
+        .appendField("Width")
+        .appendField (new Blockly.FieldNumber (600.0), "WIDTH")  
+        .appendField ( "px" );        
+        
+    this.appendDummyInput()
+        .appendField("Initial Value")
+        .appendField (new Blockly.FieldNumber (1.0), "VALUE");
+        
+    this.appendDummyInput()
+        .appendField("on input change:")
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
+
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setInputsInline(true);        
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['getelement'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("document.getElementById ")
+        .appendField (new Blockly.FieldTextInput ("id"), "ID"); 
+        
+    this.setOutput(true, null);    
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+
+Blockly.Blocks['getattribute'] = {
+  init: function() {
+
+    this.appendValueInput("OBJ");
+    
+         
+    this.appendDummyInput()
+        .appendField(".")
+        .appendField (new Blockly.FieldTextInput ("value"), "ATTRIBUTE"); 
+                
+    this.setInputsInline(true); 
+    this.setOutput(true, null);    
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+
+Blockly.Blocks['floatint'] = {
+  init: function() {
+    this.appendValueInput("OBJ")
+        .appendField("Convert ");   
+    this.appendDummyInput()
+        .appendField("to ")
+        .appendField(new Blockly.FieldDropdown([
+                                                ["float", "parseFloat"],
+                                                ["integer", "parseInt"],
+                                                ["string", "toString"]
+                                               ]), "FLOATINT");      
+
+    this.setInputsInline(true); 
+    this.setOutput(true, null);    
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['htmltext'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Add text")
+        .appendField (new Blockly.FieldTextInput ("hello"), "TEXT"); 
+    
+    this.appendDummyInput ( "Type of text" )    
+        .appendField(new Blockly.FieldDropdown([     
+                                                ["normal", "normal"],
+                                                ["heading", "heading"]
+                                               ]), "TEXTTYPE");      
+        
+    this.appendDummyInput()
+        .appendField("Size ")
+        .appendField (new Blockly.FieldNumber (1), "SIZE"); 
+                
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null); 
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['linebreak'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Add")
+        .appendField(new Blockly.FieldDropdown([
+                                                ["line break", "<br>"],
+                                                ["paragraph break", "<p>"],
+                                                ["horizontal row", "<hr>"]
+                                               ]), "LINEBREAK");      
+
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['textinput'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Text input, id:")
+        .appendField (new Blockly.FieldTextInput ("id"), "ID"); 
+                
+    this.appendDummyInput()
+        .appendField("Initial Value")
+        .appendField (new Blockly.FieldNumber (1.0), "VALUE");
+        
+    this.appendDummyInput()
+        .appendField("on change:")
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
+        
+    this.appendDummyInput()
+        .appendField("Width")
+        .appendField (new Blockly.FieldNumber (600.0), "WIDTH")  
+        .appendField ( "px" );                
+
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setInputsInline(true);        
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['div'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("DIV");
+
+    this.appendDummyInput()
+        .appendField('with border')
+        .appendField(new Blockly.FieldCheckbox(true), 'BORDER');
+
+    this.appendStatementInput("COMPONENTS")
+        .setCheck(null);
+        
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['button'] = {
+  init: function() {       
+    this.appendDummyInput()
+        .appendField("Button Text")
+        .appendField (new Blockly.FieldTextInput  ("OK"), "VALUE");
+        
+    this.appendDummyInput()
+        .appendField("on click call function:")
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
+
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setInputsInline(true);        
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['select'] = {
+  init: function() {
+        
+    this.appendDummyInput()
+        .appendField("Select (combobox)  id:")
+        .appendField (new Blockly.FieldTextInput ("id"), "ID"); 
+        
+    this.appendDummyInput()
+        .appendField("on change:")
+        .appendField (new Blockly.FieldTextInput ("funX()"), "CALL");         
+        
+    this.appendStatementInput("OPTIONS")
+        .appendField('options:')
+        .setCheck(null);        
+                                
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['option'] = {
+  init: function() {
+        
+    this.appendDummyInput()
+        .appendField("Select option:")
+        .appendField (new Blockly.FieldTextInput ("option1"), "TEXT"); 
+        
+    this.appendDummyInput()
+        .appendField("value:")
+        .appendField (new Blockly.FieldTextInput ("1"), "VALUE");         
+        
+    this.appendDummyInput()
+        .appendField('selected')
+        .appendField(new Blockly.FieldCheckbox(false), 'SELECTED');                                
+        
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setInputsInline(true);    
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['functionreturn'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Return variable")
+        .appendField (new Blockly.FieldTextInput ("x"), "VAR");
+
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['scriptinclude'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("include file")
+        .appendField (new Blockly.FieldTextInput ("utilities"), "INCLUDE");
+        
+    this.appendDummyInput()
+        .appendField ( ".js")
+
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['raw'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Raw text block")
+        .appendField(new Blockly.FieldMultilineInput('raw text'),'RAW');
+
+    // this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['expression'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Expression")
+        .appendField(new Blockly.FieldTextInput('x'),'EXPRESSION');
+
+    this.setOutput(true, null);  
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['setattribute'] = {
+  init: function() {  
+    this.appendValueInput("OBJ")
+        .appendField("Set ");
+        
+    this.appendDummyInput ()
+        .appendField ( "." )    
+        .appendField(new Blockly.FieldTextInput('attribute'),'ATTRIBUTE');
+        
+    this.appendDummyInput ()
+        .appendField ( "=" )    
+        .appendField(new Blockly.FieldTextInput('value'),'VALUE');
+        
+
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['callfunction'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Call ")
+        .appendField(new Blockly.FieldTextInput('fun1'),'NAME');
+        
+    this.appendDummyInput ()
+        .appendField ( "(" )    
+        .appendField(new Blockly.FieldTextInput('x,y'),'PARAMETERS');
+
+    this.appendDummyInput () 
+        .appendField ( ")");
+
+    this.setInputsInline(true); 
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);   
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['callfunctionreturn'] = {
+  init: function() {  
+    this.appendDummyInput()
+        .appendField("Call ")
+        .appendField(new Blockly.FieldTextInput('fun1'),'NAME');
+        
+    this.appendDummyInput ()
+        .appendField ( "(" )    
+        .appendField(new Blockly.FieldTextInput('x,y'),'PARAMETERS');
+        
+    this.appendDummyInput () 
+        .appendField ( ")");
+
+    this.setInputsInline(true); 
+    this.setOutput(true, null);  
+    this.setColour(0);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+
--- docs/demos/code/codeGenerator.js
+++ docs/demos/code/codeGenerator.js
@@ -1,4 +1,40 @@
 /*
+   Overwrites
+*/
+Blockly.JavaScript['variables_set'] = function(block) {
+  // Variable setter.
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+  return varName + ' = ' + argument0 + ';\n';
+};
+
+Blockly.JavaScript.init=function(a){
+  var temp =[];
+  Blockly.JavaScript.definitions_=Object.create(null);
+  Blockly.JavaScript.functionNames_=Object.create(null);
+  Blockly.JavaScript.variableDB_?Blockly.JavaScript.variableDB_.reset():Blockly.JavaScript.variableDB_=new Blockly.Names(Blockly.JavaScript.RESERVED_WORDS_);
+  Blockly.JavaScript.variableDB_.setVariableMap(a.getVariableMap());
+  for(var b=[],c=Blockly.Variables.allDeveloperVariables(a),d=0;d<c.length;d++)b.push(Blockly.JavaScript.variableDB_.getName(c[d],Blockly.Names.DEVELOPER_VARIABLE_TYPE));
+  a=Blockly.Variables.allUsedVarModels(a);
+  for(d=0;d<a.length;d++)
+    temp.push(Blockly.JavaScript.variableDB_.getName(a[d].getId(),Blockly.VARIABLE_CATEGORY_NAME));
+  temp.length&&(Blockly.JavaScript.variables="    // Define global variables \n    var "+temp.join(", ")+";");
+  
+  this.isInitialized=!0
+};
+
+Blockly.JavaScript.finish=function(a){
+ var b=[],c;
+ //for(c in Blockly.JavaScript.definitions_)b.push(Blockly.JavaScript.definitions_[c]);
+ //delete Blockly.JavaScript.definitions_;
+ delete Blockly.JavaScript.functionNames_;
+ Blockly.JavaScript.variableDB_.reset();
+ return b.join("\n\n")+"\n\n\n"+a
+};
+
+/*
    Utility functions 
 */
 function removeLast (line){ 
@@ -24,20 +60,37 @@ Blockly.JavaScript ['html'] = function (block) {
 
 Blockly.JavaScript ['body'] = function (block) {
   var bodyComponents = Blockly.JavaScript.statementToCode (block, 'BODY' );  
-  
+  var call = block.getFieldValue ("CALL");
+  var onloadCall = '';
+  if (call != '') {
+     onloadCall = 'onload=\"' + call + ';\"'; 
+  } 
   var code = 
-         '<body>\n' + 
+         '<body ' + onloadCall + '>\n' + 
          bodyComponents + '\n' +  
          '</body>\n'
          
   return code;
 };
 
+function getFunctionDefinitions() { 
+ var b=[],c;
+ for(c in Blockly.JavaScript.definitions_)b.push(Blockly.JavaScript.definitions_[c]);
+ delete Blockly.JavaScript.definitions_;
+ return '  // Global Function Definitions\n' + '  ' + b.join("\n\n")+"\n\n\n"; 
+} 
+
 Blockly.JavaScript ['head'] = function (block) {
+  
+
   var headComponents = Blockly.JavaScript.statementToCode (block, 'HEAD' );  
   
   var code = 
          '<head>\n' + 
+         '  <script>\n' + 
+         Blockly.JavaScript.variables + '\n' + 
+         '  ' + getFunctionDefinitions() + 
+         '  </script>\n' + 
          headComponents + '\n' +  
          '</head>\n'
          
@@ -59,7 +112,7 @@ Blockly.JavaScript ['function'] = function (block) {
   var scriptComponents = Blockly.JavaScript.statementToCode (block, 'CODE' );  
   var name = block.getFieldValue ("NAME");   
   var params = Blockly.JavaScript.statementToCode (block,'PARAMETERS');
-  params = removeLast (params);
+  params = removeLast (params);  
   var code = 'function ' + name + '(' + params + ') {\n' + 
                scriptComponents + '\n' +  
              '}\n'         
@@ -68,7 +121,7 @@ Blockly.JavaScript ['function'] = function (block) {
 
 Blockly.JavaScript ['functionparameter'] = function (block) {
   var parameter = block.getFieldValue ("PARAMETER"); 
-  var code = removeFirstLast(parameter) + ',';         
+  var code = parameter + ',';         
   return code;
 };
 
@@ -79,4 +132,178 @@ Blockly.JavaScript ['checkbox'] = function (block) {
   return code;
 };
 
+Blockly.JavaScript ['slider'] = function (block) {
+  var id = block.getFieldValue      ("ID"); 
+  var minimum = block.getFieldValue ("MINIMUM"); 
+  var maximum = block.getFieldValue ("MAXIMUM"); 
+  var width = block.getFieldValue   ("WIDTH"); 
+  var call = block.getFieldValue    ("CALL");
+  var value = block.getFieldValue   ("VALUE");
+  
+  var code = '<input style=\"width:' + width + 'px\" type=\"range\" min=\"' + minimum + '\" ' + 
+             'max=\"' + maximum + '\" value=\"' + value + '\" class=\"slider\" id=\"' + id + 
+             '\" oninput=\"' + call + ';\">\n'          
+  return code;
+};
+
+Blockly.JavaScript['getelement'] = function(block) {  
+  var id = block.getFieldValue ("ID"); 
+  var code = 'document.getElementById (' + id + ')';
+  return [code, Blockly.Python.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['getattribute'] = function(block) {  
+  var attribute = block.getFieldValue ("ATTRIBUTE"); 
+  var obj = Blockly.JavaScript.valueToCode(block, "OBJ", Blockly.Python.ORDER_ATOMIC)
+  var code = obj + '.' + attribute;
+  return [code, Blockly.Python.ORDER_NONE]; 
+};
+
+Blockly.JavaScript ['textinput'] = function (block) {
+  var id = block.getFieldValue      ("ID"); 
+  var call = block.getFieldValue    ("CALL");
+  var value = block.getFieldValue   ("VALUE");
+  var width = block.getFieldValue   ("WIDTH");
+  
+  var code = '<input style=\"width:' + width + 'px\" value=\"' + value + '\" id=\"' + id + 
+             '\" onchange=\"' + call + ';\">\n'          
+  return code;
+};
+
+Blockly.JavaScript ['div'] = function (block) {
+  var components = Blockly.JavaScript.statementToCode (block, 'COMPONENTS' );  
+  var border = block.getFieldValue ("BORDER");
+  var definition = "<div>";
+  
+  if (border == "TRUE") {
+      definition = "<div style='border:thick solid #0000FF'>";
+  } 
+    
+  var code = 
+         definition + '\n' + 
+         components + '\n' +  
+         '</div>\n'
+         
+  return code;
+};
+
+Blockly.JavaScript ['htmltext'] = function (block) {
+  var txt = block.getFieldValue ("TEXT");
+  var texttype = block.getFieldValue ("TEXTTYPE");
+  var size = block.getFieldValue ("SIZE");
+  var code;
+  
+  if (texttype == 'normal' ) {    
+     code = txt;
+  } else {
+     code = '<h' + size + '>' + txt + '</h' + size + '>\n';
+  }
+         
+  return code;
+};
+
+Blockly.JavaScript ['linebreak'] = function (block) {
+  var linebreak = block.getFieldValue ("LINEBREAK");
+  var code = linebreak;         
+  return code;
+};
+
+Blockly.JavaScript ['button'] = function (block) { 
+  var call = block.getFieldValue    ("CALL");
+  var value = block.getFieldValue   ("VALUE");
+  
+  var code = '<input type=\"button\"  value=\"' + value +  
+             '\" onclick=\"' + call + ';\">\n'          
+  return code;
+};
+
+Blockly.JavaScript ['option'] = function (block) { 
+  var txt      = block.getFieldValue ("TEXT");
+  var value    = block.getFieldValue ("VALUE");
+  var selected = block.getFieldValue ("SELECTED");
+  var sel = ''; 
+  if (selected == "TRUE") {
+     sel = 'selected';
+  }     
+  var code = '<option value=\"' + value + '\" ' + sel + '>' + txt + '</option>\n'          
+  return code;
+};
+
+Blockly.JavaScript ['select'] = function (block) {
+  var options = Blockly.JavaScript.statementToCode (block, 'OPTIONS' );  
+  var call = block.getFieldValue ("CALL");
+  var id   = block.getFieldValue ("ID");  
+  var onchange = '';
+  if (call != '') {
+      onchange = 'onchange=\"' + call + ';\"';
+  }
+  
+  var code = '<select id=\"' + id + '\" ' + onchange + '>\n' + 
+             options + '\n' +
+             '</select>\n'
+         
+  return code;
+};
+
+
+Blockly.JavaScript ['functionreturn'] = function (block) {
+  var variable = block.getFieldValue ("VAR");   
+  var code = 'return ' + variable + ';\n'         
+  return code;
+};
+
+Blockly.JavaScript ['scriptinclude'] = function (block) {
+  var include = block.getFieldValue ("INCLUDE");  
+  var code = '<script src=\"' + include + '.js\"></script>\n'        
+  return code;
+};
+
+Blockly.JavaScript ['raw'] = function (block) {
+  var raw = block.getFieldValue ("RAW");  
+  var code = raw + '\n'        
+  return code;
+};
+
+Blockly.JavaScript ['expression'] = function (block) {
+  var expression = block.getFieldValue ("EXPRESSION");  
+  var code = expression;        
+  return [code, Blockly.Python.ORDER_NONE]; 
+};
+
+Blockly.JavaScript ['setattribute'] = function (block) {
+  var obj       = block.getFieldValue ("OBJECT");  
+  var attribute = block.getFieldValue ("ATTRIBUTE");  
+  var value     = block.getFieldValue ("VALUE");  
+  var code = obj + '.' + attribute + '=' + value + ';\n'        
+  return code;
+};
+
+Blockly.JavaScript ['floatint'] = function (block) {
+  var obj = Blockly.JavaScript.valueToCode(block, "OBJ", Blockly.Python.ORDER_ATOMIC)
+  var floatint  = block.getFieldValue ("FLOATINT");  
+  var fl = '';
+  if (floatint == 'parseFloat') {
+      fl = 'parseFloat (' + obj + ')';
+  } else if (floatint == 'parseInt' ) {
+      fl = 'parseInt (' + obj + ')';
+  } else if (floatint == 'toString') {
+      fl = obj + '.toString();' 
+  } 
+  var code = fl;       
+  return [code, Blockly.Python.ORDER_NONE]; 
+};
+
+Blockly.JavaScript ['callfunction'] = function (block) {
+  var name       = block.getFieldValue ("NAME");  
+  var parameters = block.getFieldValue ("PARAMETERS");  
+  var code = name + '(' + parameters + ');\n'        
+  return code;
+};
+
+Blockly.JavaScript ['callfunctionreturn'] = function (block) {
+  var name       = block.getFieldValue ("NAME");  
+  var parameters = block.getFieldValue ("PARAMETERS");  
+  var code = name + '(' + parameters + ')'        
+  return [code, Blockly.Python.ORDER_NONE]; 
+};
 
